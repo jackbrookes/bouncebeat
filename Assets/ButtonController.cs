d700ee4a4d40d1fd176d7e76aa05f3299b7eeb18
Jack Brookes <jbrookes187@gmail.com>
2017-05-20 17:01:48 +0100

    Moved to OculusSDK, added wrist UI

---
+++ Assets/ButtonController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class ButtonController : MonoBehaviour {
+
+    public float cooldown = 0.2f;
+    public string buttonText;
+    public Button button;
+    public Text buttonTextScript;
+    public UnityEvent onPress;
+    static float refTime = 0f;
+
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (Time.time > refTime)
+        {
+            button.onClick.Invoke();
+            onPress.Invoke();
+            refTime = Time.time + cooldown;
+        }
+    }
+    
+    [ContextMenu("Update button")]
+    void UpdateButton()
+    {
+        buttonTextScript.text = buttonText;
+    }
+}
--- Assets/Scripts/HandController.cs
+++ Assets/Scripts/HandController.cs
@@ -5,10 +5,28 @@ using UnityEngine.Events;
 
 public class HandController : MonoBehaviour {
 
+    public bool flipped;
+    public GameObject handOverlay;
+    GameObject handOverlayInstance;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+        handOverlayInstance = Instantiate(handOverlay, transform);
+        if (flipped)
+        {
+
+            for (int i=0; i< handOverlayInstance.transform.childCount; i++)
+            {
+                Transform c = handOverlayInstance.transform.GetChild(i);
+                c.position = Vector3.Scale(c.position, new Vector3(-1, 1, 1));
+            }
+
+
+            RectTransform uiRect = handOverlayInstance.GetComponentInChildren<RectTransform>();
+            uiRect.Rotate(new Vector3(0, 180, 0));
+
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
