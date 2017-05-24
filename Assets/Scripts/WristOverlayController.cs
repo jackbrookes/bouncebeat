c2c6e03c5d37865bc72ac771aedd58ed5a4e2b8b
Jack Brookes <jbrookes187@gmail.com>
2017-05-24 23:50:56 +0100

    now hologram appears only when looking at wrist

---
+++ Assets/Scripts/WristOverlayController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WristOverlayController : MonoBehaviour {
+
+    public float threshold = 0f;
+    public Transform alignVector;
+    Transform head;
+    [SerializeField]
+    bool lastFrameState = false;
+
+    // Use this for initialization
+    void Start () {
+        head = GameObject.Find("CenterEyeAnchor").transform;        
+    }
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        Vector3 forward = head.TransformDirection(Vector3.forward);
+        Vector3 alligner = alignVector.TransformDirection(Vector3.forward);
+        float dot = Vector3.Dot(forward.normalized, alligner.normalized);
+
+        bool currState = dot > threshold;
+        print(dot);
+        if (lastFrameState != currState)
+        {
+            Animate(currState);
+            lastFrameState = currState;
+        }
+        
+    }
+
+    void Animate(bool state)
+    {
+        foreach(MeshRenderer render in gameObject.GetComponentsInChildren<MeshRenderer>())
+        {
+            render.enabled = state;
+        }
+        
+    }
+}
